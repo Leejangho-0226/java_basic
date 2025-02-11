1c50aac30a8e36bee4d234f412c12dd824bc415b
ljh9148 <lsh9147@naver.com>
2025-02-11 16:58:03 +0900

    Hotfix 브랜치 내용 추가

--- java_basic/src/pack/HelloTest.java
+++ java_basic/src/pack/HelloTest.java
@@ -5,6 +5,7 @@ public class HelloTest {
 	public static void main(String[] args) {
 		System.out.println("프로그램작성");
 		System.out.println("Hotfix 브랜치 내용 변경");
+		System.out.println("Hotfix 브랜치 내용 추가");
 
 	}
 
