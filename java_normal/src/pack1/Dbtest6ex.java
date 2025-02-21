2e7887fa3ed060f1ca0289e32a5c91319b958d4b
ljh9148 <lsh9147@naver.com>
2025-02-21 17:03:32 +0900

    Debugging WEB NETWORK

--- java_normal/src/pack1/Dbtest6ex.java
+++ java_normal/src/pack1/Dbtest6ex.java
@@ -1,95 +1,94 @@
 package pack1;
 
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.Scanner;
 
-
-
 public class Dbtest6ex {
+//	키보드로 직급을 입력하여 해당 직급에 해당하는 직원 자료 출력
+//	--- 출력 형태 ------------------
+//	직원번호 직원명 부서명 직급 성별
+//	... 
+//	인원 수 : *
 	private Connection conn;
 	private PreparedStatement pstmt;
 	private ResultSet rs;
-	
-	public Dbtest6ex() {
-		dbload();
-		insertData();
+
+	public Dbtest6ex() { // 생성자에서 하지 않고 별도의 메서드를 만들고 불러주는 작업을 권장함
+		dbLoad();
 		showData();
 	}
-	
-	private void dbload() {
+
+	private void dbLoad() {
 		try {
-			Class.forName("org.mariadb.jdbc.Driver");
-			conn = DriverManager.getConnection("jdbc:mariadb://192.168.0.193:3306/test","root","123");
-		}catch(Exception e) {
-			System.out.println("loading fail : " + e);
-			
+			Class.forName(("org.mariadb.jdbc.Driver"));
+		} catch (Exception e) {
+			System.out.println("loading fail: " + e);
 			System.exit(0);
 		}
 	}
-	
-	
-	private void insertData() {
-		try {
-			Scanner scanner = new Scanner(System.in);
-			
-			
-				System.out.print("직급: ");
-				String jikwonjik = scanner.next();
-			
-				
-				
-			
-			scanner.close();
-			
-			
-		}catch(Exception e) {
-			System.out.println("InsertData err : " + e);
-		}finally {
-			try {
-				if(pstmt != null) pstmt.close();
-				if(conn != null) conn.close();
-			} catch (Exception e2) {
 
-			}
-		}
-	}
-	
 	private void showData() {
+		Scanner scanner = new Scanner(System.in);
+
 		try {
-			conn = DriverManager.getConnection("jdbc:mariadb://192.168.0.193:3306/test","root","123");
-			
-			pstmt = conn.prepareStatement("select jikwonno,jikwonname,busername,jikwonjik,jikwongen from jikwon inner join buser on busernum=buserno where jikwonjik");
+			conn = DriverManager.getConnection("jdbc:mariadb://192.168.0.193:3306/test","root","123"); // 필요할 때만 DB랑
+																											// 붙는다.
+			System.out.print("직급 입력 : ");
+			String jikwonrank = scanner.next();
+
+			String sql = "select jikwonno,jikwonname,busername,jikwonjik,jikwongen "
+					+ "from jikwon left join buser "
+					+ "on busernum = buserno "
+					+ "where jikwonjik = ?";
+			pstmt = conn.prepareStatement(sql); // 선처리 방식
+			pstmt.setString(1, jikwonrank);
+
 			rs = pstmt.executeQuery();
-			
-			
-						
-			while(rs.next()) {
-				System.out.println(rs.getString(1) + " " + rs.getString(2) + " " + rs.getString(3) + " " + rs.getString(4) +" " + rs.getString(5));
-			System.out.println("인원수 :" );
+
+			int cou = 0;
+			System.out.println("직원번호 직원명 부서명 직급 성별");
+			while (rs.next()) {
+				System.out.println(
+					rs.getString("jikwonno") + " " + 
+					rs.getString("jikwonname") + " " + 
+					rs.getString("busername")+ " " + 
+					rs.getString("jikwonjik") + " " + 
+					rs.getString("jikwongen"));
+				cou++;
 			}
-		}catch(Exception e) {
-			System.out.println("showData err : " + e);
-		}finally {
+			System.out.println("인원 수 : " + cou);
+
+			System.out.print("계속할까요? (y/n)");
+			String yn = scanner.next();
+			if (yn.equals("n")) {
+				scanner.close();
+				return;
+			} else {
+				showData();
+			}
+
+		} catch (Exception e) {
+			System.out.println("showData err: " + e);
+			System.exit(0);
+		} finally { // 연결 후 끝나면 닫기
 			try {
-				if(rs != null)	rs.close();
-				if(pstmt != null) pstmt.close();
-				if(conn != null) conn.close();
+				if (rs != null)
+					rs.close();
+				if (pstmt != null)
+					pstmt.close();
+				if (conn != null)
+					conn.close();
+
 			} catch (Exception e2) {
 
 			}
 		}
 	}
-	
-	
-	
 
 	public static void main(String[] args) {
 		new Dbtest6ex();
-
 	}
-
 }
